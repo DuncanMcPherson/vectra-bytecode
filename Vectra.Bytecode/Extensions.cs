77a87c19872323b34020e95952d6090f7d3caf7f
Duncan McPherson <12081@rideuta.com>
2025-06-16 11:16:51 -0600

    refactor(extensions): add ReadString extension method
    - Introduced `ReadString` for `BinaryReader` to support reading strings.
    - Utilizes a `StringBuilder` for efficient string construction.

--- Vectra.Bytecode/Extensions.cs
+++ Vectra.Bytecode/Extensions.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace Vectra.Bytecode;
 
 internal static class Extensions
@@ -6,5 +8,16 @@ internal static class Extensions
     {
         writer.Write(value.Length);
         writer.Write(value);
+    }
+
+    public static string ReadString(this BinaryReader reader, int length)
+    {
+        var builder = new StringBuilder();
+        for (var i = 0; i < length; i++)
+        {
+            builder.Append((char)reader.ReadByte());
+        }
+        
+        return builder.ToString();
     }
 }
